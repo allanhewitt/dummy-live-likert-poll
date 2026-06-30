9dcc25530efaed1d9c7177571edee7fe86a567fe
Allan Hewitt <allanhewitt@Allans-MacBook-Air.local>
2026-06-30 16:53:43 +0100

    Switch to HashRouter to fix direct route access on static hosting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import RespondView from "./RespondView.jsx";
 import DisplayView from "./DisplayView.jsx";
 import ControlView from "./ControlView.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/respond" element={<RespondView />} />
         <Route path="/display" element={<DisplayView />} />
         <Route path="/control" element={<ControlView />} />
         <Route path="*" element={<Navigate to="/respond" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
